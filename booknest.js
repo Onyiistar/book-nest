62bd52733a6e95e0326cc0a059a84130a12d4aa4
onyiistar <michealonyedikaobinna@gmail.com>
2025-02-10 06:44:12 -0800

    first commit

---
+++ booknest.js
@@ -0,0 +1,9 @@
+function searchBooks() {
+    const searchQuery = document.getElementById('searchBar').value;
+    if (searchQuery) {
+        alert('Searching for: ' + searchQuery);
+        // Implement actual search functionality here
+    } else {
+        alert('gifted hands');
+    }
+}
